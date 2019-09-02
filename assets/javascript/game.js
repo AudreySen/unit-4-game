aa0bbf7e0fc8614a6ef2fb6b5fda60bfbbf7669d
Audrey Sengelin <audrey.sengelin@gmail.com>
2019-09-02 12:02:14 -0700

    Created four strings, initiate a loop to generate 9 seperate numbers, creates four variables with a random number for each.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
             //This code makes sure that the JavaScript doesn't get run until the HTML is finished loading          
 
-            //Un nombre a atteindre entre 19 et 120 s'affiche au hasard:
+            //Le nombre à atteindre entre 19 et 120 est généré au hasard:
             var auHasard = Math.floor(Math.random() * 120) + 19
 
-            //Pointe l'id "numeroAatteindre", donné au hasard, dans mon HTML:
+            //Pointe l'id "numeroAatteindre", généré au hasard, dans mon HTML:
             $("#numeroAatteindre").text(auHasard);
             console.log(auHasard)
-                //Je déclare trois variables pour créer le compteur:
+
+            //Je déclare trois variables pour créer le compteur:
             var scoreTotal = 0;
             var scoreGagne = 0;
             var scorePerdu = 0;
@@ -15,40 +16,54 @@ $(document).ready(function() {
             //Pointe ces id dans mon html:
             $("#gagne").text(scoreGagne);
             $("#perdu").text(scorePerdu);
-            $("#resultat").text(scoreResultat);
 
-            // Ces methodes indiqueront à l'écran le score total, les points gagnés et perdus:
-            scoreTotal.textContent = total;
+            // Ces methodes indiqueront à l'écran les points gagnés et perdus:
             scoreGagne.textContent = gagne;
             scorePerdu.textContent = perdu;
 
-            //Ai cree 4 variables avec un chiffre au hasard entre 1 et 12:
-            var valeurRouge = Math.floor(Math.random() * 12) + 1;
-            var valeurBleu = Math.floor(Math.random() * 12) + 1;
-            var valeurJaune = Math.floor(Math.random() * 12) + 1;
-            var valeurVert = Math.floor(Math.random() * 12) + 1;
-
-            //Stocker les valeurs de chaque cristal le temps de la partie (mais ne pas l'afficher dans mon html):
-
             // Chaque fois que le joueur clique une touche, la valeur de chaque cristal (un nombre au hasard entre 1 et 12) s'additionne sur le compteur id "additionClics":
+            // !!!!!!!! Ou essayer cette methode: !!!!!!!!!!!!!!
+            //$("button").click(function(){
+            //$("p").hide();
 
-            $(".rouge").on("click", function() {
+            $("#boutonRouge").on("click", function() {
+                $("button").hide();
+            });
+
+            $("#boutonBleu").on("click", function() {
+                $("button").hide();
+            });
+
+            $("#boutonJaune").on("click", function() {
+                $("button").hide();
+            });
+
+            $("#boutonVert").on("click", function() {
+                $("button").hide();
+            });
+
+            //Create 4 strings which will hold crystals numbers (ne pas les afficher dans mon html):
+            var valeurRouge = "";
+            var valeurBleu = "";
+            var valeurJaune = "";
+            var valeurvert = "";
+
+            //Then initiate a loop to generate 9 separate numbers:
+            for (var i = 0; i < 9; i++) {
+
+                //Ai crée 4 variables avec un chiffre au hasard entre 1 et 12:
+                var valeurHasardR = Math.floor(Math.random() * 12) + 1;
+                var valeurHasardB = Math.floor(Math.random() * 12) + 1;
+                var valeurHasardJ = Math.floor(Math.random() * 12) + 1;
+                var valeurHasardV = Math.floor(Math.random() * 12) + 1;
 
 
 
-                    $(".bleu").on("click", function() {
 
-                        }
+                //If statement: Si le chiffre donné est atteint, le joueur gagne, un message "You won" s'affiche et un et un point s'ajoute au un compteur score gagnés:
+                //Si le chiffre donné est dépassé, le joueur perd, un message "You lost" s'affiche et un point s'ajoute au un compteur score perdu:
 
-
-
-
-                    })
-
-                //If statement: Si le chiffre donné est atteint, le joueur gagne et un point s'ajoute au un compteur score gagnés:
-                //Si le chiffre donné est dépassé, le joueur perd et un point s'ajoute au un compteur score perdu:
-
-
+                $("#resultat").text(phraseResultat);
                 // Nouveau nombre à deviner:
                 // Nouvelles valeurs des differents cristaux:
 
